dc0586e30fe3d8f20f2fcf20faeefe868a4631ce
TJ Ram <tjram500@gmail.com>
2020-11-21 21:19:05 -0800

    added search url

--- fitbet/users/urls.py
+++ fitbet/users/urls.py
@@ -1,4 +1,4 @@
-from users.views import dashboard, register, home, profile, house
+from users.views import dashboard, register, home, profile, house, search
 from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', register, name='register'),
     path('profile/<str:id>', profile, name='profile'),
+    path('search/', search, name='search')
 ]
